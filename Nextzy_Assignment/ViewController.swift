fe54fcb46023d516fa873b6ae888dd9d1581e083
prapawity <protfc16@gmail.com>
2020-02-12 20:42:15 +0700

    ✅ init project

---
+++ Nextzy_Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nextzy_Assignment
+//
+//  Created by Prapawit Patthasirivichot on 12/2/2563 BE.
+//  Copyright © 2563 Prapawit Patthasirivichot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Nextzy_Assignment' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Nextzy_Assignment
+
+end
